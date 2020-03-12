a016f33b64ef28a45d1757de2dfb5418942bc623
ThomasAckermann <ackermann.th@outlook.com>
2020-03-12 22:33:02 +0100

    added getTrueValues function

--- bin/database.py
+++ bin/database.py
@@ -12,35 +12,32 @@ class db():
         length = sum(1 for l in data)
         data.close()
         tensordata = tf.Variable(tf.zeros([length, 2], tf.string))
-
         data = open(self.path, "r")
         i = 0
         for line in data:
             line = str(line)
-            # print(line)
             z = line.split(sep = '/')
-            # z[0] = z[0].decode('utf-8')
             z[1] = z[1].rstrip('\n')
-            # print(z)
             tensordata[i].assign(z)
             i += 1
-
         # print(tensordata)
-
-        # Pisser muss weg
         data.close()
-
         return tensordata
 
-    def loadaudio(self, index_arr): # index_arr
+    def loadAudio(self, index_arr): # index_arr
         index_len = len(index_arr)
         audio_tensor = tf.Variable(tf.zeros([index_len, 16000], tf.int32))
         for index in index_arr:
             path = "../data/train/audio/" + str(self.tensordata[index,0].numpy(), "utf-8") + "/" + str(self.tensordata[index,1].numpy(), "utf-8")
             fs, data = wavfile.read(path)
             audio_tensor[index].assign(data)
-        # print(fs)
-        # audio_tensor = tf.Variable(data)
-        print(audio_tensor)
         return audio_tensor
+
+    def getTrueValues(self, index_arr):
+        index_len = len(index_arr)
+        true_value_tensor = tf.Variable(tf.zeros([index_len], tf.string))
+        for index in index_arr:
+            true_value_tensor[index].assign(self.tensordata[index, 0])
+        return true_value_tensor
+
 
--- bin/main.py
+++ bin/main.py
@@ -4,4 +4,7 @@ import database
 if __name__ == "__main__":
     db = database.db()
     index_arr = [0,1,2]
-    db.loadaudio(index_arr)
+    audio_tensor = db.loadAudio(index_arr)
+    true_values = db.getTrueValues(index_arr)
+    print(audio_tensor)
+    print(true_values)
